f796e3edaf0a8b1ed92fe713025f63ed2bea9d46
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2024-11-27 09:28:32 +0530

    Sample Collected Place Changes
    Sample Collected Place Changes

--- LISCareRepository/Interface/ISampleCollectionRepository.cs
+++ LISCareRepository/Interface/ISampleCollectionRepository.cs
@@ -1,9 +1,12 @@
-﻿using LISCareDTO.SampleCollectionPlace;
+﻿using LISCareDTO;
+using LISCareDTO.SampleCollectionPlace;
 
 namespace LISCareRepository.Interface
 {
     public interface ISampleCollectionRepository
     {
         List<SampleCollectedAtResponse> GetSampleCollectedPlace(string partnerId);
+        APIResponseModel<object> AddSampleCollectedPlaces(SampleCollectedRequest sampleCollected);
+        APIResponseModel<object> RemoveSamplePlace(int recordId, string partnerId);
     }
 }
