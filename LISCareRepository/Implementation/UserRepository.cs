4c13ee0038877e46b1f50d15434b3a4b14e710fc
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-09-02 16:08:56 +0530

    User repository code Changes
    User repository code Changes

--- LISCareRepository/Implementation/UserRepository.cs
+++ LISCareRepository/Implementation/UserRepository.cs
@@ -134,16 +134,12 @@ namespace LISCareReposotiory.Implementation
                 while (reader.Read())
                 {
                     response.UserId = Convert.ToInt32(reader[ConstantResource.UserId]);
-                    response.FullName = Convert.ToString(reader[ConstantResource.FullName]);
-                    var userPassword = Common.Base64Decode(Convert.ToString(reader[ConstantResource.UserPassword]));
+                    response.FullName = Convert.ToString(reader[ConstantResource.FullName]) ?? string.Empty;
+                    var userPassword = Common.Base64Decode(Convert.ToString(reader[ConstantResource.UserPassword]) ?? string.Empty);
                     response.Password = userPassword;
                     response.RoleId = Convert.ToInt32(reader[ConstantResource.UserRoleId]);
-                    response.Email = Convert.ToString(reader[ConstantResource.UserEmail]);
-                    //  response.UserLogo = Configuration[ConstantResources.AppSettingsbaseUrl] + Convert.ToString(reader["UserLogo"]);
-                    response.MobileNumber = Convert.ToString(reader[ConstantResource.PhoneNumber]);
-                    //  string token = jwtSecurityToken.GenerateToken(loginResponse, this.Configuration);
-                    //loginResponse.Token = token;
-                    //loginResponse.Expires_in = Convert.ToDateTime(ConstantResources.tokenExpirationTime);
+                    response.Email = Convert.ToString(reader[ConstantResource.UserEmail]) ?? string.Empty;
+                    response.MobileNumber = Convert.ToString(reader[ConstantResource.PhoneNumber]) ?? string.Empty;
                 }
 
             }
@@ -225,30 +221,28 @@ namespace LISCareReposotiory.Implementation
                 while (reader.Read())
                 {
                     response.UserId = Convert.ToInt32(reader[ConstantResource.UserId]);
-                    response.FullName = Convert.ToString(reader[ConstantResource.FullName]);
-                    response.FirstName = Convert.ToString(reader[ConstantResource.LISUserFirstName]);
-                    response.LastName = Convert.ToString(reader[ConstantResource.LISUserLastName]);
-                    var userPassword = Common.Base64Decode(Convert.ToString(reader[ConstantResource.UserPassword]));
+                    response.FullName = Convert.ToString(reader[ConstantResource.FullName]) ?? string.Empty;
+                    response.FirstName = Convert.ToString(reader[ConstantResource.LISUserFirstName]) ?? string.Empty;
+                    response.LastName = Convert.ToString(reader[ConstantResource.LISUserLastName]) ?? string.Empty;
+                    var userPassword = Common.Base64Decode(Convert.ToString(reader[ConstantResource.UserPassword]) ?? string.Empty);
                     response.Password = userPassword;
                     response.RoleId = Convert.ToInt32(reader[ConstantResource.UserRoleId]);
-                    response.Email = Convert.ToString(reader[ConstantResource.UserEmail]);
-                    response.RoleType = Convert.ToString(reader[ConstantResource.RoleName]);
+                    response.Email = Convert.ToString(reader[ConstantResource.UserEmail]) ?? string.Empty;
+                    response.RoleType = Convert.ToString(reader[ConstantResource.RoleName]) ?? string.Empty;
                     response.IsMobileVerified = Convert.ToBoolean(reader[ConstantResource.IsMobileVerified]);
                     response.IsEmailVerified = Convert.ToBoolean(reader[ConstantResource.IsEmailVerified]);
                     //  response.UserLogo = Configuration[ConstantResources.AppSettingsbaseUrl] + Convert.ToString(reader["UserLogo"]);
-                    response.MobileNumber = Convert.ToString(reader[ConstantResource.PhoneNumber]);
+                    response.MobileNumber = Convert.ToString(reader[ConstantResource.PhoneNumber]) ?? string.Empty;
                     response.DepartmentId = Convert.ToInt32(reader[ConstantResource.DepartmentId]);
-                    response.UserStatus = Convert.ToString(reader[ConstantResource.UserStatus]);
-                    if (reader[ConstantResource.UserLogoPrefix] != "")
+                    response.UserStatus = Convert.ToString(reader[ConstantResource.UserStatus]) ?? string.Empty;
+
+                    if (Convert.ToString(reader[ConstantResource.UserLogoPrefix]) != "")
                     {
                         var imgPath = _uploadImagePath.FolderPath + Convert.ToString(reader[ConstantResource.UserLogo]);
                         var profileImage = Common.ConvertImageToBase64(imgPath);
                         var finalUserImage = reader[ConstantResource.UserLogoPrefix] + profileImage;
                         response.UserLogo = finalUserImage;
                     }
-                    //  string token = jwtSecurityToken.GenerateToken(loginResponse, this.Configuration);
-                    //loginResponse.Token = token;
-                    //loginResponse.Expires_in = Convert.ToDateTime(ConstantResources.tokenExpirationTime);
                 }
 
             }
@@ -591,22 +585,22 @@ namespace LISCareReposotiory.Implementation
                     command.ExecuteScalar();
                     OutputParameterModel parameterModel = new OutputParameterModel
                     {
-                        ErrorMessage = Convert.ToString(outputErrorMessageParm.Value),
+                        ErrorMessage = Convert.ToString(outputErrorMessageParm.Value) ?? string.Empty,
                         IsError = outputErrorParm.Value as bool? ?? default,
                         IsSuccess = outputBitParm.Value as bool? ?? default,
                     };
 
                     if (parameterModel.IsSuccess)
                     {
-                        response.Status = true;
+                        response.Status = parameterModel.IsSuccess;
                         response.StatusCode = (int)HttpStatusCode.OK;
-                        response.ResponseMessage = ConstantResource.Success;
+                        response.ResponseMessage = parameterModel.ErrorMessage;
                     }
                     else
                     {
                         response.StatusCode = (int)HttpStatusCode.NotFound;
-                        response.Status = false;
-                        response.ResponseMessage = ConstantResource.Failed;
+                        response.Status = parameterModel.IsError;
+                        response.ResponseMessage = parameterModel.ErrorMessage; 
 
                     }
                 }
@@ -816,19 +810,19 @@ namespace LISCareReposotiory.Implementation
                 {
                     LoginResponseModel loginResponse = new LoginResponseModel();
                     loginResponse.UserId = Convert.ToInt32(reader[ConstantResource.UserId]);
-                    loginResponse.FullName = Convert.ToString(reader[ConstantResource.FullName]);
-                    var userPassword = Common.Base64Decode(Convert.ToString(reader[ConstantResource.UserPassword]));
+                    loginResponse.FullName = Convert.ToString(reader[ConstantResource.FullName]) ?? string.Empty;
+                    var userPassword = Common.Base64Decode(Convert.ToString(reader[ConstantResource.UserPassword]) ?? string.Empty);
                     loginResponse.Password = userPassword;
                     loginResponse.RoleId = Convert.ToInt32(reader[ConstantResource.UserRoleId]);
-                    loginResponse.Email = Convert.ToString(reader[ConstantResource.UserEmail]);
-                    loginResponse.RoleType = Convert.ToString(reader[ConstantResource.RoleType]);
-                    loginResponse.RoleName = Convert.ToString(reader[ConstantResource.RoleName]);
+                    loginResponse.Email = Convert.ToString(reader[ConstantResource.UserEmail]) ?? string.Empty;
+                    loginResponse.RoleType = Convert.ToString(reader[ConstantResource.RoleType]) ?? string.Empty;
+                    loginResponse.RoleName = Convert.ToString(reader[ConstantResource.RoleName]) ?? string.Empty;
                     loginResponse.IsMobileVerified = Convert.ToBoolean(reader[ConstantResource.IsMobileVerified]);
                     loginResponse.IsEmailVerified = Convert.ToBoolean(reader[ConstantResource.IsEmailVerified]);
                     //  response.UserLogo = Configuration[ConstantResources.AppSettingsbaseUrl] + Convert.ToString(reader["UserLogo"]);
-                    loginResponse.MobileNumber = Convert.ToString(reader[ConstantResource.PhoneNumber]);
-                    loginResponse.UserStatus = Convert.ToString(reader[ConstantResource.UserStatus]);
-                    loginResponse.PartnerId = Convert.ToString(reader[ConstantResource.PartnerId]);
+                    loginResponse.MobileNumber = Convert.ToString(reader[ConstantResource.PhoneNumber]) ?? string.Empty;
+                    loginResponse.UserStatus = Convert.ToString(reader[ConstantResource.UserStatus]) ?? string.Empty;
+                    loginResponse.PartnerId = Convert.ToString(reader[ConstantResource.PartnerId]) ?? string.Empty;
                     response.Add(loginResponse);
                 }
 
@@ -912,7 +906,7 @@ namespace LISCareReposotiory.Implementation
                     command.Parameters.Add(new SqlParameter(ConstantResource.ParamUserLogoPrefix, userlogoPrefix.Trim()));
                     command.Parameters.Add(new SqlParameter(ConstantResource.ParmCreatedById, partnerUser.CreatedById.Trim()));
 
-                   
+
                     // output parameters
                     SqlParameter outputBitParm = new SqlParameter(ConstantResource.IsSuccess, SqlDbType.Bit)
                     {
