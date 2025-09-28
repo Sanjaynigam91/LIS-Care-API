c2742becb6caf4d93894dd034219c99be88971aa
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-09-28 12:52:37 +0530

    Analyzer master code Changes
    Analyzer master code Changes

---
+++ LISCareBussiness/Interface/IAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿using LISCareDTO;
+using LISCareDTO.AnalyzerMaster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISCareBussiness.Interface
+{
+    public interface IAnalyzer
+    {
+        Task<APIResponseModel<List<AnalyzerResponse>>> GetAllAnalyzerDetails(string partnerId, string? AnalyzerNameOrShortCode = "", string? AnalyzerStatus = "");
+    }
+}
---
+++ LISCareLimited/Controllers/AnalyzerController.cs
@@ -0,0 +1,53 @@
+﻿using LISCareBussiness.Interface;
+using LISCareDTO;
+using LISCareDTO.AnalyzerMaster;
+using LISCareDTO.ProfileMaster;
+using LISCareUtility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LISCareLimited.Controllers
+{
+    [Route(ConstantResource.APIRoute)]
+    [ApiController]
+    public class AnalyzerController(IConfiguration configuration,IAnalyzer analyzer) : ControllerBase
+    {
+        private readonly IConfiguration _configuration = configuration;
+        private readonly IAnalyzer _analyzer = analyzer;
+
+        /// <summary>
+        /// used to get all analyzer details
+        /// </summary>
+        /// <param name="partnerId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route(ConstantResource.GetAllAnalyzers)]
+        public async Task<IActionResult> GetAllAnalyzersDetails([FromQuery] string partnerId, string? AnalyzerNameOrShortCode = "", string? AnalyzerStatus = "")
+        {
+            var response = new APIResponseModel<List<AnalyzerResponse>>
+            {
+                Data = []
+            };
+            try
+            {
+                response = await _analyzer.GetAllAnalyzerDetails(partnerId,AnalyzerNameOrShortCode,AnalyzerStatus);
+                if (response.Data == null || response.Data.Count == 0)
+                {
+                    response.Status = false;
+                    response.StatusCode = StatusCodes.Status404NotFound;
+                    response.ResponseMessage = "No analyzers details found for the given PartnerId.";
+                }
+                return StatusCode(response.StatusCode, response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = StatusCodes.Status500InternalServerError;
+                response.ResponseMessage = $"An error occurred while processing your request: {ex.Message}";
+                return StatusCode(StatusCodes.Status500InternalServerError, response);
+            }
+        }
+
+
+    }
+}
