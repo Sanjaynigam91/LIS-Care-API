1e9ba525b2945136c2974802bdf394ae3d8a6250
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-10-17 21:31:20 +0530

    LIScare .CSProj File changes
    LIScare .CSProj File changes

--- LISCareLimited/obj/Debug/net8.0/LISCareLimited.AssemblyInfo.cs
+++ LISCareLimited/obj/Debug/net8.0/LISCareLimited.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LISCareLimited")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a19a28f894546e660cbcefc17917d533e9dac8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce918cd2dd4eb70dcb04c24f4bf929436011a7a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LISCareLimited")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LISCareLimited")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
