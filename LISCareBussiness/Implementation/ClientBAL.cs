971324dbc364170b4301933b8ebad8770d11f59b
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-10-21 14:22:09 +0530

    Get All Clients API Changes
    Get All Clients API Changes

---
+++ LISCareBussiness/Implementation/ClientBAL.cs
@@ -0,0 +1,37 @@
+﻿using LISCareBussiness.Interface;
+using LISCareDTO;
+using LISCareDTO.ClientMaster;
+using LISCareRepository.Implementation;
+using LISCareRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISCareBussiness.Implementation
+{
+    public class ClientBAL(IClientRepository clientRepository):IClient
+    {
+        public readonly IClientRepository _clientRepository= clientRepository;
+        /// <summary>
+        /// used to get all clients
+        /// </summary>
+        /// <param name="clientStatus"></param>
+        /// <param name="searchBy"></param>
+        /// <param name="partnerId"></param>
+        /// <param name="centerCode"></param>
+        /// <returns></returns>
+        public async Task<APIResponseModel<List<ClientResponse>>> GetAllClients(bool clientStatus, string? searchBy, string partnerId, string? centerCode)
+        {
+            try
+            {
+                return await _clientRepository.GetAllClients(clientStatus,searchBy,partnerId,centerCode);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ LISCareDTO/ClientMaster/ClientResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISCareDTO.ClientMaster
+{
+    public class ClientResponse
+    {
+        public string ClientId {  get; set; }=string.Empty;
+        public string PartnerId {  get; set; }=string.Empty;
+        public string ClientCode {  get; set; }=string.Empty;
+        public string CenterName {  get; set; }=string.Empty;
+        public string ClientName { get; set; } = string.Empty;
+        public string MobileNumber { get; set; } = string.Empty;
+        public string City {  get; set; } = string.Empty;
+        public bool IsAccessEnabled {  get; set; }=false;
+        public string UserId {  get; set; }=string.Empty;
+        public bool ClientStatus {  get; set; }=false;
+        public string LicenseNumber {  get; set; }=string.Empty;
+        public decimal DiscountPercentage {  get; set; }=decimal.MaxValue;
+        public string AdditionalContact {  get; set; }=string.Empty;
+        public string ClientType {  get; set; }=string.Empty;
+        public string BillingType {  get; set; }=string.Empty;
+    }
+}
