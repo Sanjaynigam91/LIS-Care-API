0b99d22a9f0969b40d61153e57ac47771c348e04
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-10-25 08:06:31 +0530

    Out Lab Code changes
    Out Lab Code changes

---
+++ LISCareBussiness/Interface/IOutLab.cs
@@ -0,0 +1,23 @@
+﻿using LISCareDTO;
+using LISCareDTO.OutLab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISCareBussiness.Interface
+{
+    public interface IOutLab
+    {
+        /// <summary>
+        /// used to get out labs details
+        /// </summary>
+        /// <param name="labStatus"></param>
+        /// <param name="labname"></param>
+        /// <param name="labCode"></param>
+        /// <param name="partnerId"></param>
+        /// <returns></returns>
+        Task<APIResponseModel<List<OutLabResponse>>> GetAllOutLabs(bool? labStatus, string? labname, string? labCode, string partnerId);
+    }
+}
