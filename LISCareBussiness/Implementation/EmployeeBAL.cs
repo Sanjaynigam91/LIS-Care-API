94b41415e6d4b74e339faf71d3fe58a6d4b25206
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-10-28 12:51:28 +0530

    Employee master code Changes
    Employee master code Changes

---
+++ LISCareBussiness/Implementation/EmployeeBAL.cs
@@ -0,0 +1,28 @@
+﻿using LISCareBussiness.Interface;
+using LISCareDTO;
+using LISCareDTO.MetaData;
+using LISCareRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISCareBussiness.Implementation
+{
+    public class EmployeeBAL(IEmployeeRepository employeeRepository) : IEmployee
+    {
+        private readonly IEmployeeRepository _employeeRepository = employeeRepository;
+        public async Task<APIResponseModel<List<MetaDataTagsResponseModel>>> GetEmployeeDepartments(string? category, string partnerId)
+        {
+            try
+            {
+                return await _employeeRepository.GetEmployeeDepartments(category, partnerId);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ LISCareLimited/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using LISCareBussiness.Interface;
+using LISCareDTO;
+using LISCareDTO.ClientMaster;
+using LISCareDTO.MetaData;
+using LISCareUtility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LISCareLimited.Controllers
+{
+    [Route(ConstantResource.APIRoute)]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployee _employee;
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(IEmployee employee, ILogger<EmployeeController> logger)
+        {
+            _employee = employee;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route(ConstantResource.GetEmployeeDepartment)]
+        public async Task<IActionResult> GetEmployeeDepartments([FromQuery] string? category, string partnerId)
+        {
+            _logger.LogInformation($"GetEmployeeDepartment, API execution started at:{DateTime.Now}");
+            var response = new APIResponseModel<List<MetaDataTagsResponseModel>>
+            {
+                Data = []
+            };
+            try
+            {
+                response = await _employee.GetEmployeeDepartments(category,partnerId);
+                if (response.Data == null || response.Data.Count == 0)
+                {
+                    response.Status = false;
+                    response.StatusCode = StatusCodes.Status404NotFound;
+                    response.ResponseMessage = "No departments found!";
+                }
+                _logger.LogInformation($"GetEmployeeDepartment, API execution completed at:{DateTime.Now}");
+                return StatusCode(response.StatusCode, response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = StatusCodes.Status500InternalServerError;
+                response.ResponseMessage = $"An error occurred while processing your request: {ex.Message}";
+                _logger.LogInformation($"GetEmployeeDepartment, API execution failed at:{DateTime.Now} with response {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, response);
+            }
+        }
+
+    }
+}
