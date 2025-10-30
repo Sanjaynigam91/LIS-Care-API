78b2dafec8d49fb28571abb128e5992913062079
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-10-30 13:26:13 +0530

    Barcode manger code Changes
    Barcode manger code Changes

---
+++ LISCareDTO/Barcode/BarcodeResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISCareDTO.Barcode
+{
+    public class BarcodeResponse
+    {
+        public string GeneratedOn {  get; set; }=string.Empty;
+        public int SequenceStart {  get; set; }=0;
+        public int SequenceEnd { get; set; } =0;
+        public string CreatedBy {  get; set; }=string.Empty;
+        public int GenerateId {  get; set; }=0;
+    }
+}
