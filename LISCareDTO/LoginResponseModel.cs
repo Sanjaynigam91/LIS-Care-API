67394e905b0fefa97a2d5d716594e2528a3b8a1b
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-12-25 10:35:58 +0530

    Added Center Code in login Response
    Added Center Code in login Response

--- LISCareDTO/LoginResponseModel.cs
+++ LISCareDTO/LoginResponseModel.cs
@@ -4,22 +4,23 @@
     {
         public int UserId { get; set; }
         public int RoleId { get; set; }
-        public string FullName { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-        public string MobileNumber { get; set; }
-        public string RoleType { get; set; }
-        public string RoleName { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string MobileNumber { get; set; } = string.Empty;
+        public string RoleType { get; set; } = string.Empty;
+        public string RoleName { get; set; } = string.Empty;
         public bool IsMobileVerified { get; set; }
         public bool IsEmailVerified { get; set; }
-        public string Token {  get; set; }
+        public string Token { get; set; } = string.Empty;
         public DateTime Expires_in {  get; set; }
-        public string UserStatus { get; set; }
-        public string PartnerId {  get; set; }
+        public string UserStatus { get; set; } = string.Empty;
+        public string PartnerId { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; }=string.Empty;
         public int DepartmentId { get;set; }
-        public string UserLogo {  get; set; }
+        public string UserLogo { get; set; } = string.Empty;
+        public string CenterCode { get; set; }= string.Empty;
 
 
 
